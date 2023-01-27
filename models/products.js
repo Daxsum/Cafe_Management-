4bc2632b7cad3863e525465f42b9dbc6431bdf19
Daxsum <kaldax250@gmail.com>
2023-01-27 13:00:31 +0300

    fix:fix some required variables to not required and adding aditional variable to fix reletional issu

--- models/products.js
+++ models/products.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 const Joi = require("joi");
 const { TypeSchema } = require("./types");
 const ProductSchema = new mongoose.Schema({
-  filePath: { type: String, required: true },
+  filePath: { type: String },
   name: { type: String, required: true },
-  type: { type: TypeSchema, required: true },
+  type: { type: TypeSchema },
   numberInStock: { type: Number, required: true },
   price: { type: Number, required: true },
 });
@@ -13,7 +13,7 @@ function validate(item) {
   //validation
   const schema = Joi.object({
     name: Joi.string().required().min(2),
-    typeId: Joi.string().required().min(2),
+    typeId: Joi.string(),
     numberInStock: Joi.number().required().min(1),
     price: Joi.number().required().min(1),
   });
